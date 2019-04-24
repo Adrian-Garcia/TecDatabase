f85598906174f0961a758777451f95401e22953f
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-04-24 11:38:02 -0500

    Added new tables

--- TecUniversity.sql
+++ TecUniversity.sql
@@ -9,3 +9,39 @@
 
 --First line to create the database 
 CREATE Database TecUniversity;
+
+/*
+Departamento(id, nombre, numero, telefono, oficina)
+	PK(id)
+*/
+
+create table Department(
+    dId varchar(20) Not Null,
+    dName varchar (50) Not Null,
+    dNumber varchar(5) Not Null,
+    dPhoneNumber varchar(20) Not Null,
+    dOffice varchar(20) Not Null,
+    primary key(dId)
+)Engine=InnoDB;
+
+/*
+Alumnos(Matrícula, curp, nombre, dirección, teléfono, sexo, fechaDeNacimiento, pertenece, carrera)
+PK(Matrícula, curp)					Grado 		9
+FK(pertenece) references Departamento(iD)		FK		1
+
+*/
+
+
+create table Students(
+    sID varchar(20) Not Null,
+    UPRC varchar(20) NOT Null, --UPRC == CURP
+    sName varchar(25) Not Null,
+    sAddress varchar(25)  Null, 
+    sPhoneNumber varchar (20) Null, 
+    sSex varchar (1) Null,  
+    sBDate datetime(6) null, 
+    sDepartment varchar(20) Not null, 
+    sMajor varchar(20) Not Null,
+    primary key(sID), 
+    foreign key(sDepartment) references Departament(dId)
+)Engine=InnoDB;
