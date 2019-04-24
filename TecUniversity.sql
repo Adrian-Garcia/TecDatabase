182344de98cee8ef918e32b35826eedd98c93d8c
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-04-24 10:41:05 -0500

    Add files via upload

---
+++ TecUniversity.sql
@@ -0,0 +1,11 @@
+/*
+    Tec Database 
+    Creators:
+    David A. Cantú Martínez 	A00822455
+    Adrián García López 	    A01351166 
+    Paola A. Villarreal García 	A00821971
+    Luis Adrian Gartner López   A00227224
+*/
+
+--First line to create the database 
+CREATE Database TecUniversity;
