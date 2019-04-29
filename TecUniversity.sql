152fb94a9fc8be7ca16ddd50d34900f33ad4ad07
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-04-29 18:32:45 -0500

    Updated the Database with new Tables.
    Added tables takecourse, course and groupp.

--- TecUniversity.sql
+++ TecUniversity.sql
@@ -9,8 +9,8 @@
 
 --First line to create the database 
 CREATE Database TecUniversity;
-
-create table Department(
+--Creating Tables 
+CREATE TABLE Department(
     dId varchar(20) Not Null,
     dName varchar (50) Not Null,
     dNumber varchar(5) Not Null,
@@ -19,37 +19,26 @@ create table Department(
     primary key(dId)
 )Engine=InnoDB;
 
-/*
-Alumnos(Matrícula, curp, nombre, dirección, teléfono, sexo, fechaDeNacimiento, pertenece, carrera)
-    PK(Matrícula, curp)                             Grado		9
-    FK(pertenece) references Departamento(iD)		FK			1
-*/
-create table Students(
+CREATE TABLE Students(
     sID varchar(20) Not Null,
     sUPRC varchar(20) NOT Null, --UPRC == CURP
     sName varchar(25) Not Null,
     sAddress varchar(25)  Null, 
     sPhoneNumber varchar (20) Null, 
     sSex varchar (1) Null,  
-    sBDate datetime(6) null, 
-    sDepartment varchar(20) Not null, 
+    sBDate datetime(6) Null, 
+    sDepartment varchar(20) Not Null, 
     sMajor varchar(20) Not Null,
     primary key(sID, UPRC), 
     foreign key(sDepartment) references Department(dId)
 )Engine=InnoDB;
 
 CREATE TABLE Schedule(
-    hId varchar(25) not null,
-    hTime datetime(6) not null,
+    hId varchar(25) Not Null,
+    hTime datetime(6) Not Null,
     primary key(hId)
 )Engine=InnoDB;
 
-/*
-Profesor(id    , horarioDisponible)
-    PK(id)                            				Grado		1
-	FK(tiene) references Curso(id)            		FK			2
-	FK(tiene) references Grupo(número)
-*/
 CREATE TABLE Professor(
     pID varchar(20) Not Null,
     pUPRC varchar(20) NOT Null, --UPRC == CURP
@@ -57,42 +46,50 @@ CREATE TABLE Professor(
     pAddress varchar(25)  Null, 
     pPhoneNumber varchar (20) Null, 
     pSex varchar (1) Null,  
-    pBDate datetime(6) null, 
+    pBDate datetime(6) Null, 
     primary key(pID)
 )Engine=InnoDB; 
 
 CREATE TABLE ProfessorSchedule(
      pID varchar(20) Not Null,
-     hId varchar(25) not null,
+     hId varchar(25) Not Null,
      primary key(pId, hId), 
      foreign key (pID) references Professor(pId),
      foreign key (hId) references Schedule(hId)
 )Engine=InnoDB;
 
-/*
-TomaCurso(Matrícula, curp, número, calificación, historial)        
-    PK(Matrícula, curp, número)                    	Grado		4
-    FK(NúmeroGrupo) references Grupo(NúmeroGrupo)	FK        	2
-    FK(NúmeroCurso) references Grupo(NúmeroCurso)
-*/
-create table TakeCourse(
-	cID varchar(20) Not Null,
-	cUPRC varchar(20) Not Null,
-	cNumber varchar(5) Not Null,
-	cNote varchar(2) Not Null,
-	cHistory varchar(25) Not Null,
-	primary key(cID)
+CREATE TABLE Course(
+    cNumber varchar(20) Not Null,
+    cName varchar(25) Not Null,
+    cDescription varchar(25) Not Null, 
+    cHours datetime(6) Not Null,
+    dId varchar(20) Not Null,
+    primary key(cNumber),
+    foreign key(dId) references Department(dId)
 )Engine=InnoDB;
 
-/*
-Grupo(Matricula, curp, ecoa, año, semestre, número)
-    PK(número)                        				Grado		6
-    FK(tiene) references Profesor(id)           	FK 			2
-    FK(tiene) references Curso(número)
-*/
+CREATE TABLE Groupp(
+    gId varchar(20) Not Null,
+    gNumber varchar(20) Not Null,
+    pID varchar(20) Not Null,
+    cNumber varchar(20) Not Null,
+    gEcoa varchar(20) Not Null, 
+    gUPRC varchar(20) Not Null,
+    gYear datetime(6) Not Null,
+    gSemester varchar(20) Not Null,
+    primary key(gNumber, gId, gUPRC),
+    foreign key(pId) references Professor(pId),
+    foreign key(cNumber) references Course(cNumber) 
+)Engine=InnoDB;
 
-/*
-Curso(número, nombre, descripción, horas)
-    PK(número)                        				Grado		1
-    FK(ofrece) references Departamento(id)         	FK			3
-*/
+create table TakeCourse(
+	tcID varchar(20) Not Null,
+	tcUPRC varchar(20) Not Null,
+	tcGroupNumber varchar(5) Not Null,
+    tcCourseNumber varchar(5) Not Null,
+	tcGrade varchar(2) Not Null,
+	tcHistory varchar(25) Not Null,
+	primary key(tcID),
+    foreign key(tcCourseNumber) references Course(cNumber),
+    foreign key(tcGroupNumber) references Groupp(gNumber)
+)Engine=InnoDB;
